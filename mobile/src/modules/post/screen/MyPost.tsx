9e80d24063e9d6e48a3d564cbf545f18871afb77
phongduong213 <haiphongduong69@gmail.com>
2026-03-24 22:15:22 +0700

    Post screen

---
+++ mobile/src/modules/post/screen/MyPost.tsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import MobileLayout from '../../../components/MobileLayout/MobileLayout'
+import { View } from 'react-native'
+
+const MyPost = () => {
+    return (
+        <MobileLayout title="Tin của tôi">
+            {/* Nội dung của MyPost sẽ được thêm vào đây */}
+            <View>
+
+            </View>
+        </MobileLayout>
+    )
+}
+
+export default MyPost
